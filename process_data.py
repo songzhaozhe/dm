b01705b11f7273d3a430013a17da429ab0a666aa
songzz <songzhaozhe@126.com>
2017-05-23 17:14:54 +0800

    finish preprocessing

--- process_data.py
+++ process_data.py
@@ -1,11 +1,23 @@
+# -*- coding: utf-8 -*-
 import pandas as pd
 import numpy as np
 import os
-#['LastPrice'#0, 'LastVolume'#1, 'LastTurnover'#2, 'AskPrice1'#3, 'BidPrice1'#4, 'AskVolume1'#5, 'BidVolume1'#6, #7, #8]
-def process_label(file_name, id, needed_columns):
-    STEP = 20
+#['LastPrice'#0, 'LastVolume'#1, 'LastTurnover'#2, 'AskPrice1'#3, 'BidPrice1'#4, 'AskVolume1'#5, 'BidVolume1'#6, #7]
+
+def get_path():
+    return "./data/m0000"
+def get_STEP():
+    return 20
+def get_needed_columns():
+    #return ['LastPrice', 'LastVolume', 'LastTurnover', 'AskPrice1', 'BidPrice1', 'AskVolume1', 'BidVolume1','BidVolume2','BidVolume3']
+    return ['LastPrice', 'LastVolume', 'LastTurnover', 'AskPrice1', 'BidPrice1', 'AskVolume1', 'BidVolume1',
+            'BidVolume2']
+
+def process_label(file_name, id):
+    STEP = get_STEP()
     dF = pd.read_csv(file_name)
-    day_matrix = dF.as_matrix(needed_columns)
+    print(file_name)
+    day_matrix = dF.as_matrix(get_needed_columns())
     row_num = day_matrix.shape[0]
     last_row_num = row_num  # 表示label只能到第几行，否则搜到底也没有合适的label了
     dp = np.zeros(row_num)  # 往后推是涨了（1）还是跌了（0）。-1只会出现在末尾一串
@@ -34,20 +46,24 @@ def process_label(file_name, id, needed_columns):
             else:
                 day_matrix[i, -1] = dp[j]
     day_matrix = day_matrix[0:last_row_num]
+    path = get_path()
+    file = open(os.path.join(path, '%d.out' %id), 'w')
+    np.save(file,day_matrix)
+    file.close()
+
 
 
 def main():
-    const_interval = 20
-    needed_columns = ['LastPrice', 'LastVolume', 'LastTurnover', 'AskPrice1', 'BidPrice1', 'AskVolume1', 'BidVolume1','BidVolume2','BidVolume3']
-    path = "./data/m0000"
+    path = get_path()
     files = os.listdir(path)
     for file_name in os.listdir(path):
         if file_name.endswith(".csv"):
             files.append(os.path.join(path, file_name))
     files.sort()
-
-    for id in range(len(files)):
-        process_label(files[id], id, needed_columns)
+    tot = len(files)
+    #tot = 1
+    for id in range(tot):
+        process_label(files[id], id)
 
 #['LastPrice'#0, 'LastVolume'#1, 'LastTurnover'#2, 'AskPrice1'#3, 'BidPrice1'#4, 'AskVolume1'#5, 'BidVolume1'#6, #7, #8]
 if __name__ == '__main__':
