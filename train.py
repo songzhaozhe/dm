28f3315c31e98d80f4005f275bc1bb508ea2cde8
qu10wenhao <shark_010@163.com>
2017-05-25 19:59:51 +0800

    rnn

--- train.py
+++ train.py
@@ -5,30 +5,38 @@ from __future__ import division
 from __future__ import print_function
 
 import os
-
+import random
 import numpy as np
 import tensorflow as tf
 from keras.models import Sequential
-from keras.layers import Dense, Activation
+from keras.layers import Dense, Activation, LSTM, Masking
 from keras.models import model_from_json
-from sklearn.preprocessing import normalize
+from sklearn.preprocessing import scale
+from keras.optimizers import Adam
 
-model = Sequential()
-model.add(Dense(32, input_dim=784))
-model.add(Activation('relu'))
+#model = Sequential()
+#model.add(Dense(32, input_dim=784))
+#model.add(Activation('relu'))
 
-os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
-os.environ["CUDA_VISIBLE_DEVICES"] = "7"
+#os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+#os.environ["CUDA_VISIBLE_DEVICES"] = "7"
 # Data sets
 
 path = "./data/m0000"
 save_path = "./models/"
 input_size = 8
+time_step = 40
+
 
 class dataset():
     def __init__(self, data, target):
         self.data = data
         self.target = target
+
+class indexset():
+	def __init__(self,file_index, row_index):
+		self.file_index = file_index
+		self.row_index = row_index
 
 def load_dataset():
     files = []
@@ -44,33 +52,111 @@ def load_dataset():
         #print(tmp.shape)
         all_list.append(tmp)
 
-
+    print("concatenating matrix....")
     all_matrix = np.concatenate(all_list, axis=0)
-    all_data = all_matrix[:,0:-1]
-    all_target = all_matrix[:,-1]
-    all_data = normalize(all_matrix, norm='l2',axis = 0, return_norm = True)
-    print(norms)
-
     tot_row = all_matrix.shape[0]
-    train_matrix = all_matrix[0:int(tot_row*0.7)]
-    test_matrix = all_matrix[int(tot_row*0.7):]
-    train_set = dataset(train_matrix)
-    test_set = dataset(test_matrix)
+    print("slicing matrix...")
+    all_data = all_matrix[:,:-1]
+    #print ('all_Data',len(all_data[0]))
+    all_target = all_matrix[:,-1]
+    #print(np.mean(all_data, axis = 0))
+
+    all_data = scale(all_data, axis = 0)
+    train_data = np.array(all_data[0:int(tot_row*0.7),:]).reshape(int(tot_row*0.7),time_step,input_size)
+    train_target = np.array(all_target[0:int(tot_row*0.7)]).reshape(int(tot_row*0.7),time_step,1)
+    test_data = np.array(all_data[int(tot_row*0.7):,:]).reshape(tot_row-int(tot_row*0.7),time_step,input_size)
+    test_target = np.array(all_target[int(tot_row*0.7):]).reshape(tot_row-int(tot_row*0.7),time_step,1)
+
+    train_set = dataset(train_data, train_target)
+    test_set = dataset(test_data, test_target)
     return train_set, test_set
 
+def prepare_data_for_generator(files):
+	all_list = []
+	len_list = []
+	index_list = []
+	feature_list = []
+	label_list = []
+	for i in range(len(files)):
+		tmp = np.load(files[i])
+        #print(tmp.shape)
+		for j in range(len(tmp)-time_step+1):
+			index_list.append(indexset(i,j))
+		len_list.append(len(tmp))
+		all_list.append(tmp[:,:-1])
+		label_list.append(tmp[:,-1])
+	all_matrix = np.concatenate(all_list, axis=0)
+	print("slicing matrix...")
+	all_data = scale(all_matrix, axis = 0)
+	index = 0
+	for i in range(len(files)):
+		feature_list.append(all_data[index:index+len_list[i],:])
+		index = index + len_list[i]
+
+	feature_list = np.array(feature_list)
+	label_list = np.array(label_list)
+
+	return index_list, feature_list, label_list
+
+def data_generator(index_list, feature_list, label_list, batch_size = 128):
+
+    random.shuffle(index_list)
+    maxlen = len(index_list)
+    current = 0
+    batch_features = np.zeros([batch_size,time_step,input_size])
+    batch_label = np.zeros([batch_size,1])
+    while True:
+    	#batch_features = []
+    	#batch_label = []
+    	if current + batch_size <= maxlen:
+    		for i in range(current,current+batch_size):
+    			fi = index_list[i].file_index
+    			ri = index_list[i].row_index
+    			batch_features[i%batch_size] = feature_list[fi][ri:ri+time_step,:]
+    			batch_label[i%batch_size] = label_list[fi][ri+time_step-1]
+    		current = current + batch_size
+    	else:
+    		nextbatch = current + batch_size - maxlen;
+    		for i in range(current,maxlen):
+    			fi = index_list[i].file_index
+    			ri = index_list[i].row_index
+    			batch_features[i%batch_size] = feature_list[fi][ri:ri+time_step,:]
+    			batch_label[i%batch_size] = label_list[fi][ri+time_step-1]
+    		random.shuffle(index_list)
+    		current = 0
+    		for i in range(current, nextbatch):
+    			fi = index_list[i].file_index
+    			ri = index_list[i].row_index
+    			batch_features[batch_size - nextbatch + i%batch_size] = feature_list[fi][ri:ri+time_step,:]
+    			batch_label[batch_size - nextbatch + i%batch_size] = label_list[fi][ri+time_step-1]
+    		current = nextbatch
+    	yield batch_features, batch_label
+
+
+
+data_files = []
+for file_name in os.listdir(path):
+    if file_name.endswith(".out"):
+        data_files.append(os.path.join(path, file_name))
+data_files.sort()
+
 model = Sequential()
-model.add(Dense(32, input_dim = input_size, activation = 'relu'))
-model.add(Dense(32, activation = 'relu'))
-model.add(Dense(32, activation = 'relu'))
+model.add(Masking(mask_value= 0,	input_shape=(time_step, input_size)))
+model.add(LSTM(16, input_shape = (time_step,input_size), activation = 'relu',return_sequences=True))
+model.add(LSTM(32, activation = 'relu',return_sequences=True))
+model.add(LSTM(16, activation = 'relu',return_sequences=False))
 model.add(Dense(1, activation='sigmoid'))
-model.compile(optimizer='rmsprop',
+adam = Adam(lr = 0.001)
+model.compile(optimizer=adam,
               loss='binary_crossentropy',
               metrics=['accuracy'])
-train_set, test_set = load_dataset()
-model.fit(train_set.data, train_set.target , epochs=2, batch_size=128)
+#train_set, test_set = load_dataset()
+#model.fit(train_set.data, train_set.target , epochs=2, batch_size=128, shuffle = False)
 
-score = model.evaluate(test_set.data, test_set.target, batch_size=128)
-print(score)
+index_list, feature_list, label_list = prepare_data_for_generator(data_files)
+model.fit_generator(data_generator(index_list, feature_list, label_list,128),epochs=20, steps_per_epoch=16368)
+#score = model.evaluate(test_set.data, test_set.target, batch_size=128)
+#print(score)
 
 save_file = os.path.join(path, 'model.json')
 save_weights_file = os.path.join(path, 'model.h5')
